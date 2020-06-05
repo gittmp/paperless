60471595f5a75bfb1f1a10fb3ab4d19064057b1c
gittompotter <tom.potter@durham.ac.uk>
2020-06-05 16:47:46 +0100

    added alternative news source - can restrict inputs

--- client/index.js
+++ client/index.js
@@ -2,23 +2,80 @@ async function getTopNews(size, country){
 
     let url = 'https://newsapi.org/v2/top-headlines?apiKey=cb527766d5474d5ebcccd51023d032bf&country=' + country + '&pagesize=' + String(size);
 
-    let result = await fetch(url)
-        .then(function (response) {
-            return response.json();
+    let result = await fetch(url, {
+        method: 'GET'
         })
+        .then(response => response.json())
+        .catch((error) => {
+            console.error('Error:', error);
+            return error;
+    });
 
-    return result
+    return result;
 }
 
-async function getPreview(obj){
+async function getTopNews2(limit, country){ //%20OR%20title%3AUK
+    let url = 'https://api.newsriver.io/v2/search?query=language%3AEN%20AND%20(website.domainName%3Abbc.co.uk)&sortBy=discoverDate&sortOrder=DESC&limit=' + String(limit);
+    const token = 'sBBqsGXiYgF0Db5OV5tAwypVCIPW_sVl7GCQx0RUezHZZuTSmzmITA0VmFNNAWN0n2pHZrSf1gT2PUujH1YaQA'
+    
+    let result = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': token,
+        }})
+        .then(response => response.json())
+        .catch((error) => {
+            console.error('Error:', error);
+            let index = String(error).indexOf(":");
+            return {
+                'status': "error",
+                'code': String(error).substring(0, index),
+                'message': String(error).substring(index+2)
+            };
+    });
+
     return {
-        'source': obj.source.name,
-        'author': obj.author,
-        'title': obj.title,
-        'desc': obj.description,
-        'url': obj.url,
-        'image': obj.urlToImage,
+        'status': "ok",
+        'articles': result
     }
+}
+
+async function getPreview(obj, source){
+    let news = {};
+
+    if(source == 0){
+
+        news['source'] = obj.source.name;
+        news['title'] = obj.title;
+        news['desc'] = obj.description;
+        news['url'] = obj.url;
+        news['image'] = obj.urlToImage;
+        // news['time'] = obj.
+
+    } else if(source == 1){
+
+        news['source'] = obj.website.name;
+        news['title'] = obj.title;
+        news['url'] = obj.url;
+        news['time'] = obj.discoverDate;
+
+        let elems = obj.elements;
+        if(elems.length != 0){
+            news['image'] = elems[0].url;
+        } else {
+            news['image'] = "";
+        }
+
+        let text = obj.text;
+        let index = text.indexOf(".");
+        if(index != -1){
+            news['desc'] = text.substring(0, index);
+        } else {
+            news['desc'] = text;
+        }
+    }
+
+    return news;
 }
 
 const topicList = [
@@ -49,6 +106,7 @@ const biasChart = {
     "Wales Online": 'Left',
     "The Observer": 'Slight Left',
     "Sky News": 'Center',
+    "Sky.com": 'Center',
     "The Financial Times": 'Center',
     "Financial Times": 'Center',
     "BBC News": 'Center',
@@ -59,12 +117,27 @@ const biasChart = {
     "The Daily Express": 'Right',
     "Express": 'Right',
     "Express.co.uk": 'Right',
-    "Daily Mail": 'Far Right'
+    "Daily Mail": 'Far Right',
+    "The Daily Mail": 'Far Right',
 }
 
-async function loadPage(size, country){
+async function loadPage(size, country, source){
 
-    let news = await getTopNews(size, country);
+    let d = new Date();
+    let time = d.getHours() + '.' + d.getMinutes() + '.' + d.getSeconds();
+    let date = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+
+    let news = {
+        'status': "error"
+    }
+
+    if(source == 0){
+        news = await getTopNews(size, country);
+    } else if(source == 1){
+        news = await getTopNews2(size, country);
+    }
+
+    console.log(news);
     let html = '';
 
     if(news.status == "ok"){
@@ -87,13 +160,13 @@ async function loadPage(size, country){
             for(let j = 0; j < lim; j++){
 
                 let article = news.articles[k];
-                let preview = await getPreview(article);
+                let preview = await getPreview(article, source);
 
                 html += '<div class="articles">';
                 html += '<a target="_blank" href="' + preview.url + '">';
                 html += '<img src="' + preview.image + '" alt="article' + k + 'image" class="image" width="600" height="400">';
                 html += '<div class="desc" style="font-weight: bold; font-size: 14px;">' + preview.title + '</div>';
-                html += '<div class="desc">' + preview.desc + '</div>';
+                html += '<div class="desc">' + preview.desc + ' (' + preview.time + ')</div>';
                 html += '<div class="desc">Source: ' + preview.source + '<br>';
 
                 if(preview.source in biasChart){
@@ -112,6 +185,7 @@ async function loadPage(size, country){
         html += '<strong>Error!</strong><br><strong>Code:</strong> ' + news.code + '<br><strong>Message:</strong> ' + news.message;
     }
 
-    document.getElementById("content").innerHTML = html;
+    document.getElementById("content").innerHTML = html;    
+    document.getElementById("head").innerHTML += '<pre>Updated:  ' + time + '  ' + date + '</pre>';
 
 }
